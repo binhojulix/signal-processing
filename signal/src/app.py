7baffd9dff319054b2c436c6e157a1c5cc8aacb5
binhojulix <fabio.jdluz@gmail.com>
2022-03-13 10:37:01 -0300

    commit

--- signal/src/app.py
+++ signal/src/app.py
@@ -1,1 +1,8 @@
-print("hello world")
+from numpy import linspace, arange
+from scipy.stats import norm
+from matplotlib import pyplot as plt
+
+x = linespace(-3,3,100)
+y = norm.pdf(x)
+plf.clf()
+plt.plot(x, y)
